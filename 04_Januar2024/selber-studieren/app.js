8d9a6dc4a072704c71594e9d5704684422cc09ad
Abdulxd <tartilaci@gmail.com>
2024-01-10 21:22:35 +0100

    übung_macht_weiter

---
+++ 04_Januar2024/selber-studieren/app.js
@@ -0,0 +1,29 @@
+
+
+// let sayac=0
+
+// while (sayac <= 10) {
+//     console.log(sayac);
+//     sayac++
+// }
+
+
+// let sayac=0;
+
+// while (sayac <= 20) {
+//     if (sayac % 2 == 0) {
+//         console.log(sayac);
+//     }
+//     sayac++
+// }
+
+
+let sayac1 = 1;
+while (true) {
+    console.log(sayac1);
+    if (sayac1==5) {
+        break;
+    }
+    sayac1 ++;
+}
+
