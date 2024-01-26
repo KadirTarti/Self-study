3bd18531a46acf954c4bb82a594a39f06fc54d22
Abdulxd <tartilaci@gmail.com>
2024-01-26 10:33:44 +0100

    add mouse-on-over

--- 04_Januar2024/26.01.2024_JS_DOM-qSelector/events.js
+++ 04_Januar2024/26.01.2024_JS_DOM-qSelector/events.js
@@ -12,17 +12,60 @@ document.querySelector("title").textContent="DOM MANIPULATION"
 
 
 js.onmouseover=()=>{
-    badi.style.backgorunColor='pink'
+    badi.style.backgroundColor='pink'
 }
 
 js.onmouseout=()=>{
-    badi.style.backgorunColor="red"
+    badi.style.backgroundColor="lightgray"
 }
+
+
+
 
 
 
 //* example2  onclick  - ondoubleclick
 
+const bir = document.querySelector(".bir")
+const iki = document.querySelector(".iki")
 
-const bir 
+bir.onclick=()=>{
+    bir.src="./img/logo2.png"
+    iki.src="./img/js_logo.png"
+}
+
+iki.onclick=()=>{
+    bir.src="./img/js_logo.png"
+    iki.src="./img/logo2.png"
+}
+
+iki.ondblclick=()=>{
+    iki.src="./img/bir.jpeg"
+}
+
+
+
+
+
+ 
+//* example3    genelde çok tercih edilmez
+
+const buton = document.querySelector("#btn")
+
+buton.addEventListener("click", () => {
+    badi.style.backgroundImage="linear-gradient(to right, lightblue, lightpink)"
+    
+    document.querySelector('#par').style.fontSize="30px"
+
+    document.querySelector('.bir').style.width="200px"
+
+    buton.textContent="TIKLANDI"
+});
+
+
+
+
+
+
+//* example4  mouse on -  mouse over
 
