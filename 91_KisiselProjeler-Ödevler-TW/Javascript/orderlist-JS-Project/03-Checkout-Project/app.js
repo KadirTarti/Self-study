e27c172f6bc706213493d93e980f327fd9537731
Abdulxd <tartilaci@gmail.com>
2024-02-08 09:41:30 +0100

    start project

---
+++ 91_KisiselProjeler-Ödevler-TW/Javascript/orderlist-JS-Project/03-Checkout-Project/app.js
@@ -0,0 +1,12 @@
+const container = document.createElement('div');
+
+container.innerHTML = 'This is a dynamically added element';
+container.style.color = 'blue';
+
+document.body.appendChild(container);
+
+const imgContainer = document.createElement('div');
+document.container.appendChild(imgContainer);
+
+const imgElement = document.createElement('img');
+document.imgContainer.appendChild(imgElement)
