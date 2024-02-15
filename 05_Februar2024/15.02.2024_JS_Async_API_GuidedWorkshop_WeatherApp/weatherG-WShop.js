269191bf779dd6f274635347b7fa376bc0db3557
Abdulxd <tartilaci@gmail.com>
2024-02-15 14:51:32 +0100

    weather app

--- 05_Februar2024/15.02.2024_JS_Async_API_GuidedWorkshop_WeatherApp/weatherG-WShop.js
+++ 05_Februar2024/15.02.2024_JS_Async_API_GuidedWorkshop_WeatherApp/weatherG-WShop.js
@@ -1,30 +1,45 @@
-function fetchWeatherData() {
-  const cityName = document.getElementById('city-input').value;
-  const apiKey = 'f66ce15566f9ac8343e1c6598fef2fff'; // Replace with your API key
+async function fetchWeatherData() {
+  const cityName = document.getElementById("city-input").value.toLowerCase();
+  console.log(cityName);
+  const apiKey = "f66ce15566f9ac8343e1c6598fef2fff"; // Replace with your API key
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
+  try {
+    const response = await fetch(url); // network
+    const data = await response.json(); // promise
+    console.log("result", data);
 
-  new Date().toLocaleDateString(); // 11/16/2015
+    const date = new Date().toDateString();
+    const weatherInfo = `${date} ${cityName.toUpperCase()} Temperature: ${
+      data.main.temp} °C`;
 
-  fetch(url)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          return response.json();
-      })
-      .then(data => {
-          document.getElementById('weather-info').textContent = `${new Date().toLocaleDateString()} ${cityName.toUpperCase()} Temperature: ${data.main.temp} °C`;
-      })
-      .catch(error => {
-          console.error('Error fetching weather data:', error);
-      });
+   
+
+
+    document.getElementById("weather-info").textContent = weatherInfo;
+    const weatherIcon = document.querySelector('.weather-icon img');
+    weatherIcon.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    weatherIcon.style.display = 'block';
+
+
+    var z = document.createElement('div');
+    z.innerHTML = `The wind value in ${cityName} is ${data.wind.speed}`;
+    const weatherWind = document.querySelector('.weather-icon').appendChild(z)
+
+
+ // data.wind.speed
+
+
+    // data.sys.country
+    // data.weather[0].icon
+    //http://openweathermap.org/img/w/01d.png
+  } catch (err) {
+    console.log("err", err);
+  }
 }
 
-document.getElementById('city-input').addEventListener('keypress', function(event) {
-  if (event.key === 'Enter') {
-      fetchWeatherData();
-    }
-  });
-
-  document.getElementById('search-button').addEventListener('click', fetchWeatherData);
+document.onload = () => {
+  document
+    .querySelector(".search-button")
+    .addEventListener("click", fetchWeatherData);
+};
