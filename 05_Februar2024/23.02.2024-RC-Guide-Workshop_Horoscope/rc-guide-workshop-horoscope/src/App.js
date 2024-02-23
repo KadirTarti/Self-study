4243d6a8953da81d8dc0902b1a423244a12913e7
Abdulxd <tartilaci@gmail.com>
2024-02-23 14:24:31 +0100

    add files

---
+++ 05_Februar2024/23.02.2024-RC-Guide-Workshop_Horoscope/rc-guide-workshop-horoscope/src/App.js
@@ -0,0 +1,21 @@
+//! burada kullanılacak tüm sayfalar üste import edilmek ZORUNDA!
+
+
+import React from 'react';
+import './App.scss';
+import Navbar1 from './components/navbar/Navbar';
+
+const App = () => {
+  return (
+    <div>
+    
+    <h1>23.02.2024 - REACT - HOROSCOPE PROJECT</h1>
+
+    <Navbar1/>
+    
+    </div>
+  )
+}
+
+export default App
+
