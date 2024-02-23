f49c1a75a13c3490970a64bc0e80f844b11f27f5
Abdulxd <tartilaci@gmail.com>
2024-02-23 15:02:00 +0100

    add navbar links

--- 05_Februar2024/23.02.2024-RC-Guide-Workshop_Horoscope/rc-guide-workshop-horoscope/src/components/navbar/Navbar.jsx
+++ 05_Februar2024/23.02.2024-RC-Guide-Workshop_Horoscope/rc-guide-workshop-horoscope/src/components/navbar/Navbar.jsx
@@ -7,25 +7,17 @@ const Navbar1 = () => {
     <div>
       <Navbar expand="lg" className="bg-body-tertiary container">
         <Container className='konteynir' fluid>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+          <a href="#home"><img src="" alt="" /></a>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+            <Nav className="links ms-auto">
+              <Nav.Link href="#home">HOROSCOPE</Nav.Link>
+              <Nav.Link href="#link">DAILY</Nav.Link>
+              <Nav.Link href="#link">TAROT</Nav.Link>
+              <Nav.Link href="#link">ARTICLE</Nav.Link>
+              <Nav.Link href="#link">CONTACT</Nav.Link>
+              
             </Nav>
           </Navbar.Collapse>
         </Container>
