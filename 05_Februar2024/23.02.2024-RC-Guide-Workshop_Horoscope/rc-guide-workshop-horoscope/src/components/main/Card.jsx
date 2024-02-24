46b850aa6443a2ded31b53b1139c2075f3a7e6ad
Abdulxd <tartilaci@gmail.com>
2024-02-24 14:04:42 +0100

    fix card articles

--- 05_Februar2024/23.02.2024-RC-Guide-Workshop_Horoscope/rc-guide-workshop-horoscope/src/components/main/Card.jsx
+++ 05_Februar2024/23.02.2024-RC-Guide-Workshop_Horoscope/rc-guide-workshop-horoscope/src/components/main/Card.jsx
@@ -1,4 +1,4 @@
-import {Card, Button} from 'react-bootstrap';
+import {Card, Button, Col, Row} from 'react-bootstrap';
 
 import './Main.scss'
 
@@ -6,9 +6,10 @@ const Card1 = ({veri}) => {
   console.log(veri);
 
   return (
-    <div>
+    <div className='d-flex flex-wrap'>
     {veri.map(({id, title, desc, image}) =>{
       return (
+        <Col className='col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center' key={id}>
       <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image}/>
       <Card.Body>
@@ -19,6 +20,7 @@ const Card1 = ({veri}) => {
         <Button variant="primary">{title}</Button>
       </Card.Body>
     </Card>
+      </Col>
       )
     })}
     </div>
