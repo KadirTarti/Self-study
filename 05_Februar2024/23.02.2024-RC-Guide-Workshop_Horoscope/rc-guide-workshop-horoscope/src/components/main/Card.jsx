b5ed60def110c1f5fdd4a8118926a9e51940e353
Abdulxd <tartilaci@gmail.com>
2024-02-25 12:38:48 +0100

    change const card

--- 05_Februar2024/23.02.2024-RC-Guide-Workshop_Horoscope/rc-guide-workshop-horoscope/src/components/main/Card.jsx
+++ 05_Februar2024/23.02.2024-RC-Guide-Workshop_Horoscope/rc-guide-workshop-horoscope/src/components/main/Card.jsx
@@ -8,16 +8,19 @@ const Card1 = ({veri}) => {
   return (
     <div className='d-flex flex-wrap'>
     {veri.map(({id, title, desc, image}) =>{
-      return (
+      (
         <Col className='col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center' key={id}>
       <Card className='cards' style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image}/>
       <Card.Body>
         <Card.Title>{id}</Card.Title>
+        <div className="card-text-wrapper" style={{ maxHeight: '200px', overflowY: 'hidden' }}>
+               
         <Card.Text className="card-text overflow-auto" style={{ height: '200px'}}>
           {desc}
         </Card.Text>
-        <Button variant="primary">{title}</Button>
+        </div>
+        <Button className="card-over" variant="primary">{title}</Button>
       </Card.Body>
     </Card>
       </Col>
